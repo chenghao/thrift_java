0cb1fcffef0223da54d4a24516286483667b5336
chenghao <hao2432@gmail.com>
2014-06-12 15:25:35 +0800

    init

--- src/com/hao/demo/client/Client.java
+++ src/com/hao/demo/client/Client.java
@@ -10,37 +10,38 @@ import org.apache.thrift.transport.TTransport;
 import com.hao.demo.thrift.Demo;
 import com.hao.demo.thrift.Hello;
 
-/** 
- * 类说明 
- * @author  程豪
- * @version V1.0  创建时间：2014-6-12 上午11:56:59 
+/**
+ * 类说明
+ *
+ * @author 程豪
+ * @version V1.0  创建时间：2014-6-12 上午11:56:59
  */
 public class Client {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		TTransport transport = new TFramedTransport(new TSocket("localhost", 9000));
-		TProtocol protocol = new TCompactProtocol(transport);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        TTransport transport = new TFramedTransport(new TSocket("localhost", 9000));
+        TProtocol protocol = new TCompactProtocol(transport);
 
-		Hello.Client client = new Hello.Client(protocol);
-		try {
-			transport.open();
+        Hello.Client client = new Hello.Client(protocol);
+        try {
+            transport.open();
 
-			System.out.println(client.echoHello("chenghao_java"));
-			
-			Demo demo = new Demo();
-			demo.setName("chenghao_java");
-			demo.setAge(25);
-			System.out.println(client.echoDemo(demo));
+            System.out.println(client.echoHello("chenghao_java"));
 
-		} catch (TException e) {
-			e.printStackTrace();
-		} finally {
-			// 最后一定要调用close来释放资源
-			transport.close();
-		}
-	}
+            Demo demo = new Demo();
+            demo.setName("chenghao_java");
+            demo.setAge(25);
+            System.out.println(client.echoDemo(demo));
+
+        } catch (TException e) {
+            e.printStackTrace();
+        } finally {
+            // 最后一定要调用close来释放资源
+            transport.close();
+        }
+    }
 
 }
